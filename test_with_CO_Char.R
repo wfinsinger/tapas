ba6f00ca71e84a92061cee680138de4cc649d686
Walter Finsinger <walter.finsinger@univ-montp2.fr>
2021-12-06 14:17:42 +0100

    Cleaning

--- test_with_CO_Char.R
+++ test_with_CO_Char.R
@@ -43,7 +43,7 @@ source("./R/Plot_ReturnIntervals.r")
 
 
 
-### Load data ##########################################################
+### Load data ############################################################################
 
 ### Charcoal data ###
 co <- read.csv("./Data-In/CO_charData.csv", header = T)
@@ -52,56 +52,73 @@ co <- dplyr::rename(co, "AgeBot" = "AgeBot..yr.BP.")
 co <- dplyr::rename(co, "char" = "CharCount.....")
 co$dummy_chararea <- co$char * 0.2421
 
-## Pretreatment
+## Resample data #########################################################################
 min.ages <- min(co$AgeTop)
 max.ages <- max(co$AgeTop)
 yr.interp <- 15
 co_i <- pretreatment_data(series = co, out = "accI", series.name = "co",
-                             first = -51, last = 7500, yrInterp = yr.interp)
+                             first = -50, last = 7450, yrInterp = yr.interp)
 plot(co_i$raw$series$age, co_i$raw$series$char, type = "l")
 lines(co_i$int$series.int$age, co_i$int$series.int$char, col = "red")
 
 
-## Detrending
-co.detr1 <- SeriesDetrend(series = co_i, smoothing.yr = 500,
+## Detrend the data ######################################################################
+co_detr <- SeriesDetrend(series = co_i, smoothing.yr = 500,
                             detr.type = "rob.loess", out.dir = "Figures")
-co.detr1 <- SeriesDetrend(series = co_i, smoothing.yr = 500,
+co_detr <- SeriesDetrend(series = co_i, smoothing.yr = 500,
                           detr.type = "mov.mode", out.dir = "Figures")
-#rm(char.thresh.gl1, co, co_i)
+#rm(char_thresh_gl, co, co_i)
 
-save.image(file = "./Data-Out/co_detr1.rdata")
+save.image(file = "./Data-Out/co_detr.rdata")
 
 
-## Fit gaussian mixture models to determine the noise-signal threshold
 
-char.thresh.gl1 <- glob_thresh(series = co.detr1, proxy = "charAR", thresh.yr = 500,
-                                thresh.value = 0.95, smoothing.yr = 500, span.sm = 0.2,
+## Fit gaussian mixture models to determine the noise-signal threshold ###################
+
+## With an overall (global threshold) & with minimum-count test
+char_thresh_gl <- global_thresh(series = co_detr, proxy = "charAR",
+                                thresh.value = 0.95, smoothing.yr = 500,
                                 keep_consecutive = F, minCountP = 0.05,
                                 out.dir = "Figures")
 
-char.thresh.gl1 <- glob_thresh(series = co.detr1, proxy = "charAR", thresh.yr = 500,
-                               thresh.value = 0.95, smoothing.yr = 500, span.sm = 0.2,
-                               keep_consecutive = F, minCountP = NULL,
+char_thresh_gl <- global_thresh(series = co_detr, proxy = "charAR",
+                              thresh.value = 0.95, smoothing.yr = 500,
+                              keep_consecutive = F, minCountP = 0.05,
+                              out.dir = "Figures")
+
+char_thresh_gl <- global_thresh(series = co_detr, proxy = "dummy_charareaAR",
+                              thresh.value = 0.95, smoothing.yr = 500,
+                              keep_consecutive = F, minCountP = NULL,
+                              out.dir = "Figures")
+
+## With overall threshold & w/out minimum-count test
+char_thresh_gl <- global_thresh(series = co_detr, proxy = "charAR",
+                               thresh.value = 0.95, smoothing.yr = 500,
+                               keep_consecutive = F, minCountP = 0.05,
                                out.dir = "Figures")
 
-char.thresh.loc <- local_thresh(series = co.detr1, proxy = "charAR", thresh.yr = 500,
-                                thresh.value = 0.99, smoothing.yr = 500,
-                                keep_consecutive = F,
+char_thresh_gl <- global_thresh(series = co_detr, proxy = "charAR",
+                              thresh.value = 0.95, smoothing.yr = 500,
+                              keep_consecutive = F, minCountP = 0.05,
+                              out.dir = "Figures")
+
+## With locally-fitted thresholds
+char_thresh_loc <- local_thresh(series = co_detr, proxy = "charAR", thresh.yr = 500,
+                                thresh.value = 0.95, smoothing.yr = 500,
+                                keep_consecutive = F, minCountP = 0.05,
                                 out.dir = "Figures")
 
 
 
-## Make final plots
+## Make final plots ######################################################################
 layout(1)
 par(mfrow = c(2,1), mar = c(0,5,2,0.5), oma = c(4,1,0,0))
-Plot.Anomalies(series = char.thresh.gl1, proxy = "charAR", plot.crosses = T,
+Plot.Anomalies(series = char_thresh_gl, plot.crosses = T,
                plot.x = F, plot.neg = F)
-Plot_ReturnIntervals(series = char.thresh.gl1, proxy = "charAR",
-                     plot.x = T, plot.neg = F)
+Plot_ReturnIntervals(series = char_thresh_gl, plot.x = T, plot.neg = F)
 
-Plot.Anomalies(series = char.thresh.loc, proxy = "charAR", plot.crosses = T,
+Plot.Anomalies(series = char_thresh_loc, plot.crosses = T,
                plot.x = F, plot.neg = F)
-Plot_ReturnIntervals(series = char.thresh.loc, proxy = "charAR",
-                     plot.x = T, plot.neg = F)
+Plot_ReturnIntervals(series = char_thresh_loc, plot.x = T, plot.neg = F)
 
 

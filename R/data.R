f71456027a3a6cbccde86668bddf66b593917522
Iago-lito <iago-lito@etak>
2022-01-10 17:12:21 +0100

    Integrate co_char_data.rda.

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' TODO: Describe This Data
+#'
+#' This data used for example scripts needs to be described.
+#'
+#' @format A data frame with 471 rows and 7 variables:
+#' \describe{
+#'   \item{cmTop}{Sample top in centimeters}
+#'   \item{cmBot}{Sample bottom in centimeters}
+#'   \item{AgeTop}{Age of sample Sample top}
+#'   \item{AgeBot}{Age of sample Sample bottom}
+#'   \item{charvol}{Sample volume in cm³}
+#'   \item{char}{Sample count}
+#'   \item{dummy_chararea}{dummy variable (charcoal area) to show
+#'                         that resampling and detrending is done for
+#'                         all the variables listed in the dataset
+#'                         (one column for each variable).}
+#' }
+#' @source TODO: Where does this come from? \url{?}
+"co_char_data"

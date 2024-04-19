3ee6a2ad81d5d12f76abd01084e1db679969723b
Walter Finsinger <walter.finsinger@univ-montp2.fr>
2024-04-19 14:18:25 +0200

    fixed read_imagej_results()

--- R/read_imagej_results.R
+++ R/read_imagej_results.R
@@ -1,27 +1,34 @@
 #' Reads results of charcoal analyses made with ImageJ
 #'
 #' @param folder_in Sets the path to the directory where the *.csv files are
-#'                  located (e.g. "./data_in/")
+#'                  located (e.g. "./data_in/" if the files are in a subfolder
+#'                  relatively to the working directory).
 #' @param metadata A vector that describes the metadata stored in the file names.
 #'                 By default, metadata = c("core_id", "depth", "image_id"),
 #'                 for file names formatted as "CoreID_Depth_ImageID.csv".
 #'                 However, other file name formats are possible. See the
 #'                 Description here below.
 #' @param image_id_default Specifies the value or character that shall be
-#'                          added if the file name does not include the
-#'                          information for the ImageID. By default,
+#'                          added if sometimes the file name does not include
+#'                          the information for the ImageID. By default,
 #'                          \code{image_id_default = "a"}.
+#' @param na_replace Logical. If \code{na_replace = TRUE} (default), \code{NA}
+#'                    values will be replaced with \code{0} values for all
+#'                    numeric columns.
 #'
 #' @description
 #' The function loads spreadsheets obtained with ImageJ (https://imagej.net/)
 #' that were stored as *.csv files (comma-separated) in a common folder whose
 #' path is specified by the \code{folder_in} argument.
+#'
 #' The function expects that the file names include a set of information that
 #' identify the samples unequivocally, and that the information is given as
 #' strings separated by a dash (-) or an underscore (_) (do not use empty
 #' spaces (space bar) to separate the metadata).
+#' It is recommended to use a hierarchical file-name format that includes the
+#' ImageID information last.
 #'
-#' By default the following information is expected:
+#' By default the following set of information is expected in the file names:
 #'  - a site identifier;
 #'  - a sample identifier, for instance the master core depth;
 #'  - an image identifier (a character string) in case several images were
@@ -29,8 +36,8 @@
 #'     image identifier is optional (an "a" will be added if the file name does
 #'     not include this information).
 #'
-#' However, the function allows to use a different set and number of
-#' identifiers. Possible alternatives may include:
+#' However, users can choose a different set and number of identifiers.
+#' Possible alternatives may include:
 #' - "CoreID_SectionID_Depth_ImageID.csv" or
 #' - "CoreID_SectionID_DepthInDrive_ImageID.csv".
 #'
@@ -43,20 +50,32 @@
 #' such as the chronology or other proxy data.
 #'
 #' @returns
-#' A data.frame with the specified metadata columns, and all columns that
-#'  are common to all input files.
+#' A data.frame with the specified \code{metadata} columns, and all columns
+#' that appear in any of the input files.
 #'
 #' @author Walter Finsinger
+#'
+#' @examples
+#' \dontrun{
+#' folder_in = "./data_in/",
+#' metadata = c("site_id", "depth", "image_id"),
+#' image_id_default = "a",
+#' na_replace = TRUE
+#' }
 #'
 #' @export
 #'
 #' @importFrom dplyr bind_cols
 #' @importFrom dplyr bind_rows
 #' @importFrom utils read.csv
+#' @importFrom dplyr mutate
+#' @importFrom dplyr across
+#' @importFrom tidyr replace_na
+#' @importFrom tidyselect where
 
 read_imagej_results <- function(folder_in = NULL,
                                 metadata = c("site_id", "depth", "image_id"),
-                                image_id_default = "a") {
+                                image_id_default = "a", na_replace = TRUE) {
 
   ## Read file names in the 'folder_in'
   file_names <- list.files(folder_in)
@@ -80,7 +99,6 @@ read_imagej_results <- function(folder_in = NULL,
               "is incompatible with the desired set of metadata")))
     }
 
-
     # If the file name passes the check-up, write metadata in metadata_i:
     if (length(f_ids_i) == length(metadata) - 1) {
       f_ids_i[length(f_ids_i) + 1] <- image_id_default
@@ -101,18 +119,25 @@ read_imagej_results <- function(folder_in = NULL,
     f_data_i <- read.csv(paste0(folder_in, file_names[i]))
     colnames(f_data_i)[1] <- "char_id"
 
-    ## If the file has no data but has been saved:
+    ## If a csv has no data (for samples without charcoal pieces),
+    ## add an NA row:
     if (dim(f_data_i)[1] == 0) {
-      f_data_i[1, ] <- 0
+      f_data_i[1, ] <- NA
     }
 
-    ## Add metadata to the loaded data.frame, and move it to the list:
+    ## Bind metadata to the loaded data.frame, and move it to the list:
     f_data_i <- dplyr::bind_cols(metadata_i, f_data_i)
     df_list[[i]] <- f_data_i
   }
 
   ## Gather data from list of data.frames
   df <- dplyr::bind_rows(df_list)
+
+  ## If one wants to replace the NA values
+  if (na_replace == TRUE) {
+    df <- df %>% dplyr::mutate(dplyr::across(tidyselect::where(is.numeric),
+                                      ~tidyr::replace_na(.x, 0)))
+  }
 
   ## Return output:
   return(df)
